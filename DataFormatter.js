64fb6b76c5000e96d38808b6664d6c7ce5af83ac
Michel Vorwieger <Michel.Vorwieger@mediaman.de>
2018-05-02 10:50:39 +0200

    added DataFormatter

---
+++ DataFormatter.js
@@ -0,0 +1,51 @@
+class DataFormatter {
+    static gatherByTypes(data, typesToSearchFor) {
+        return typesToSearchFor.map(type => data.filter(d => d.type === type));
+    }
+
+    static flatten(object, separator = '.') {
+
+        const isValidObject = value => {
+            if (!value) {
+                return false
+            }
+
+            const isArray  = Array.isArray(value)
+            const isObject = Object.prototype.toString.call(value) === '[object Object]'
+            const hasKeys  = !!Object.keys(value).length
+
+            return !isArray && isObject && hasKeys
+        }
+
+        const walker = (child, path = []) => {
+
+            return Object.assign({}, ...Object.keys(child).map(key => isValidObject(child[key])
+                ? walker(child[key], path.concat([key]))
+                : { [path.concat([key]).join(separator)] : child[key] })
+            )
+        }
+
+        return Object.assign({}, walker(object))
+    }
+
+    static realFlatten(specificData) {
+        /**
+         * Flatten the object
+         */
+        let modifiedData = DataFormatter.flatten(specificData);
+
+        /**
+         * split of the index numbers that were concatinated by the flattening
+         */
+        for (var key in modifiedData) {
+            if (modifiedData.hasOwnProperty(key)) {
+                modifiedData[key.split('.')[1]] = modifiedData[key];
+                delete modifiedData[key]
+            }
+        }
+
+        return modifiedData;
+    }
+}
+
+module.exports = DataFormatter;
--- app.js
+++ app.js
@@ -1,12 +1,19 @@
+//imports
+const DataExporter = require('./DataExporter.js');
+const DataFormatter = require('./DataFormatter.js');
 const Nightmare = require('nightmare');
 const nightmare = Nightmare({
-    show: true,
+    show: false,
     openDevTools: {mode: 'detach'},
     typeInterval: 1
 });
 const cheerio = require('cheerio');
+
+//move this to env props or request user input
 const username = 'rm.social@t-online.de';
 const password = 'klarastrasse';
+
+//Config
 const streamQueries = 'Activity,Weight,BloodPressure,Glucose,Nutrition,Oxygen,Sleep,Temperature';
 const offset = 0;
 const limit = 10;
@@ -14,22 +21,23 @@ const startMonthname = 'Feb';
 const startDay = 1;
 const startYear = 2018;
 const endMonthname = 'Apr';
-const endDay = 28;
+const endDay = 26;
 const endYear = 2018;
 const startDate = `${startMonthname}%20${startDay},%20${startYear}`;
 const endDate = `${endMonthname}%20${endDay},%20${endYear}`;
-let data = [];
+const uri = `https://www.fitnesssyncer.com/notebook-data?op=list&stream=${streamQueries}&taskIds=Notebook,43c4e088-4cf3-4ef0-9d7f-f710f699a3f5,99ed24a3-3b43-4b6d-9656-9882cb0a72fc,ac3271dd-b481-48f8-9642-e6d768cdb124&activityTypes=&offset=${offset}&limit=${limit}&customStartDate=${startDate}&customEndDate=${endDate}`;
+
+//Used to gather type information
+let typeArr = [];
 
 nightmare.goto('https://www.fitnesssyncer.com/sign-in')
     .type('input#u', 'rm.social@t-online.de')
     .type('input#p', 'klarastrasse')
     .click('input#signinButton')
     .wait('.content')
-    .goto(`https://www.fitnesssyncer.com/notebook-data?op=list&stream=${streamQueries}&taskIds=Notebook,43c4e088-4cf3-4ef0-9d7f-f710f699a3f5,99ed24a3-3b43-4b6d-9656-9882cb0a72fc,ac3271dd-b481-48f8-9642-e6d768cdb124&activityTypes=&offset=${offset}&limit=${limit}&customStartDate=${startDate}&customEndDate=${endDate}`)
+    .goto(uri)
     .wait('pre')
-    .evaluate(() => {
-        return document.body.innerHTML;
-    })
+    .evaluate(() => document.body.innerHTML)
     .then((data) => {
         let cData = cheerio.load(data);
         let parsedData = JSON.parse(cData('pre').text());
@@ -38,13 +46,25 @@ nightmare.goto('https://www.fitnesssyncer.com/sign-in')
             .filter(data => data !== 'Data for Stream');
 
         let exportData = pureDataArr.map(data => {
+            //Render the dom part we need with cheerio
             let pcData = cheerio.load(data);
-            let title = pcData('.title span').text();
-            let datetime = pcData('.datetime').text();
+
+            //Storage for the Data
             let specificData = [];
+
+            //Get title and datetime of activity
+            let type = pcData('.title span').text();
+            /**
+             * Gather Typenames
+             */
+            if(!typeArr.find((typeInArr) => typeInArr === type)) {
+                typeArr.push(type);
+            }
+
+            let datetime = pcData('.datetime').text();
             let specDataArr = pcData('.kpi');
 
-            specDataArr.each(function(index, data) {
+            specDataArr.each(function() {
                 specificData.push({
                     label: cData(this).find('.label').text(),
                     value: cData(this).find('.value').text(),
@@ -52,13 +72,20 @@ nightmare.goto('https://www.fitnesssyncer.com/sign-in')
                 });
             });
 
+            specificData = specificData.map(data => {
+               return {[data.label]: data.value}
+            });
+
             return {
-                title,
+                type,
                 datetime,
-                specificData
+                ...DataFormatter.realFlatten(specificData)
             }
         });
         //TODO: Add data to database
-        console.dir(exportData, {color: true, depth: 10});
+        let filteredData = DataFormatter.gatherByTypes(exportData, typeArr);
+        let exporter = new DataExporter(JSON.stringify(filteredData, null, 4));
+        exporter.exportAsJSONTo('./data.json');
+        //console.dir(exportData, {color: true, depth: 10});
     })
     .catch(err => console.error(err));
